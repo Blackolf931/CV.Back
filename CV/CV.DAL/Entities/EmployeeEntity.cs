5fc214f5c4dee4bf4358404923044c150bcd134a
Ilya Safonov <illia220011@gmail.com>
2022-07-19 15:03:42 +0300

    Removed warnings.

--- CV/CV.DAL/Entities/EmployeeEntity.cs
+++ CV/CV.DAL/Entities/EmployeeEntity.cs
@@ -9,8 +9,8 @@ namespace CV.DAL.Entities
     public class EmployeeEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Patronymic { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Patronymic { get; set; } = string.Empty;
     }
 }
