6822fdaa277ff7bb6e91bc9eb0d74ab4f5594e01
Ilya Safonov <illia220011@gmail.com>
2022-07-19 13:43:47 +0300

    Added db connection.

--- CV/CV/Program.cs
+++ CV/CV/Program.cs
@@ -1,7 +1,11 @@
+using CV.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
