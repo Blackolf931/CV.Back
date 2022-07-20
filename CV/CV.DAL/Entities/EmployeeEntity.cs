8f494c04a8d3da88ed36408f0a6744813231f6dd
Ilya Safonov <illia220011@gmail.com>
2022-07-20 11:03:54 +0300

    Added new entity. (#24)
    * Added db connection.
    
    * Added new entity
    
    * Renamed new entity.
    
    * Migrations
    
    * Removed warnings.
    
    * Used pragma warning instead default value.
    
    * Used nullable instead pragma of warning.
    
    * Changed default connection.

---
+++ CV/CV.DAL/Entities/EmployeeEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV.DAL.Entities
+{
+    public class EmployeeEntity
+    {
+        public int Id { get; set; }
+#nullable disable
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+#nullable enable
+    }
+}
