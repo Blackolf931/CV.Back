df01c6f18d5aa2511f908125403868c2ffa37a08
Ilya Safonov <illia220011@gmail.com>
2022-07-20 15:13:42 +0300

    Added project controller.

---
+++ CV/CV/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CV.API.ViewModel.Project;
+using CV.BLL.Interfaces;
+using CV.BLL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ProjectController : GenericController<ProjectViewModel, ShortProjectViewModel, AddProjectViewModel, ProjectModel, UpdateProjectViewModel>
+    {
+        public ProjectController(IGenericServices<ProjectModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
