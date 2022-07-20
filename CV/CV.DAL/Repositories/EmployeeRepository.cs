019549cfd9a99c04afede492b5d5bea9f3b6680c
max <maks.shamig.91@mail.ru>
2022-07-20 11:30:07 +0300

    added employee and project repositories

---
+++ CV/CV.DAL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV.DAL.EF;
+using CV.DAL.Entities;
+using CV.DAL.Interfaces;
+
+namespace CV.DAL.Repositories
+{
+    public class EmployeeRepository : GenericRepository<EmployeeEntity>, IGenericRepository<EmployeeEntity>
+    {
+        public EmployeeRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
