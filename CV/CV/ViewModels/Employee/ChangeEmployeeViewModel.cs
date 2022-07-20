5acc99af9154b76df91118c864373cf9e2567a02
Ilya Safonov <illia220011@gmail.com>
2022-07-20 11:28:59 +0300

    Added Employee ViewModel.

---
+++ CV/CV/ViewModels/Employee/ChangeEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CV.API.ViewModels.Employee
+{
+    public class ChangeEmployeeViewModel
+    {
+#nullable disable
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+#nullable enable
+    }
+}
---
+++ CV/CV/ViewModels/Employee/ShortEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CV.API.ViewModels.Employee
+{
+    public class ShortEmployeeViewModel
+    {
+#nullable disable
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+#nullable enable
+    }
+}
