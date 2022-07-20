e92946a93b41a5d9fd80a7fad9d5218881fae0cf
max <maks.shamig.91@mail.ru>
2022-07-20 14:40:12 +0300

    added token for async methods

--- CV/CV.DAL/Repositories/GenericRepository.cs
+++ CV/CV.DAL/Repositories/GenericRepository.cs
@@ -18,14 +18,14 @@ namespace CV.DAL.Repositories
 
         public virtual async Task<IEnumerable<TEntity>> GetAll(CancellationToken token)
         {
-            return await dbSet.AsNoTracking().ToListAsync();
+            return await dbSet.AsNoTracking().ToListAsync(token);
         }
 
         public virtual async Task<TEntity> Create(TEntity tEntity, CancellationToken token)
         {
-            await dbSet.AddAsync(tEntity);
+            await dbSet.AddAsync(tEntity, token);
 
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(token);
 
             return tEntity;
         }
@@ -34,21 +34,21 @@ namespace CV.DAL.Repositories
         {
             dbSet.Update(tEntity);
 
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(token);
 
             return tEntity;
         }
 
         public virtual async Task<TEntity?> GetById(int id, CancellationToken token)
         {
-            return await dbSet.FindAsync(new object[] { id });
+            return await dbSet.FindAsync(new object[] { id }, token);
         }
 
         public virtual async Task Delete(TEntity tEntity, CancellationToken token)
         {
             dbSet.Remove(tEntity);
 
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(token);
         }
     }
 }
