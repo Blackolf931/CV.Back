e77bbf45daf2517799937712b5d9ec9737e7c4f8
max <maks.shamig.91@mail.ru>
2022-07-20 14:29:50 +0300

    added inherit for genericrepo

--- CV/CV.DAL/Interfaces/IGenericRepository.cs
+++ CV/CV.DAL/Interfaces/IGenericRepository.cs
@@ -3,7 +3,7 @@
     public interface IGenericRepository<TEntity> where TEntity : class
     {
         Task<IEnumerable<TEntity>> GetAll(CancellationToken token);
-        Task<TEntity> GetById(int id, CancellationToken token);
+        Task<TEntity?> GetById(int id, CancellationToken token);
         Task<TEntity> Update(TEntity item, CancellationToken token);
         Task<TEntity> Create(TEntity entity, CancellationToken token);
         Task Delete(TEntity item, CancellationToken token);
