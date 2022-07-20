83b60e1f9dbe317a92290be48e23585965e726b8
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-20 15:25:46 +0300

    Add mapper for api

--- CV/CV.DAL/Repositories/ProjectRepository.cs
+++ CV/CV.DAL/Repositories/ProjectRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CV.DAL.EF;
+﻿using CV.DAL.EF;
 using CV.DAL.Entities;
 using CV.DAL.Interfaces;
 
---
+++ CV/CV/Mappers/Mapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CV.API.ViewModel.Employee;
+using CV.API.ViewModel.Project;
+using CV.BLL.Models;
+
+namespace CV.API.Mappers
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<EmployeeModel, EmployeeViewModel>().ReverseMap();
+            CreateMap<ProjectModel, ProjectViewModel>().ReverseMap();
+            CreateMap<AddEmployeeViewModel, EmployeeModel>().ReverseMap();
+            CreateMap<ShortEmployeeViewModel, EmployeeModel>().ReverseMap();
+            CreateMap<UpdateEmployeeViewModel, EmployeeModel>().ReverseMap();
+            CreateMap<ShortEmployeeViewModel, EmployeeModel>().ReverseMap();
+        }
+    }
+}
