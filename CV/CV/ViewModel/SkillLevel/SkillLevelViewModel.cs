f8a24bce519b94b25d0a5c2aa9963927da35784c
Ilya Safonov <illia220011@gmail.com>
2022-07-21 13:49:33 +0300

    Added skill level view model.

---
+++ CV/CV/ViewModel/SkillLevel/SkillLevelViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CV.API.ViewModel.SkillLevel
+{
+    public class SkillLevelViewModel
+    {
+        public int Id { get; set; }
+#nullable disable
+        public string Name { get; set; }
+#nullable enable
+    }
+}
