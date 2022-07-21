48572dbba7812cf20ec1e57c679e690d4ce0fe75
Vlad Zagorsky <fanar1n@mail.ru>
2022-07-21 15:54:41 +0300

    add-skills-repositories

---
+++ CV/CV.DAL/Repositories/SkillRepository.cs
@@ -0,0 +1,12 @@
+﻿using CV.DAL.EF;
+using CV.DAL.Entities;
+
+namespace CV.DAL.Repositories
+{
+    public class SkillRepository : GenericRepository<SkillEntity>
+    {
+        public SkillRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
