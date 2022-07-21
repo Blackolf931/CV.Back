82e3be2a7067098361b62fb69a8f4bbbf10932bf
AngryKirill <mister.lolich@mail.ru>
2022-07-21 13:05:20 +0300

    Add Skill context and migration

--- CV/CV.DAL/Migrations/20220721100448_AddSkill.Designer.cs
+++ CV/CV.DAL/Migrations/20220721100448_AddSkill.Designer.cs
@@ -4,6 +4,7 @@ using CV.DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CV.DAL.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220721100448_AddSkill")]
+    partial class AddSkill
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
