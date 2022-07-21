83626701cf47012186a8490d07bc0c2f701f3b08
max <maks.shamig.91@mail.ru>
2022-07-21 13:28:49 +0300

    add skill,skill group,skill level models

---
+++ CV/CV.BLL/Models/SkillGroupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV.BLL.Models
+{
+    public class SkillGroupModel
+    {
+#nullable disable
+        public string Name { get; set; }
+#nullable enable
+    }
+}
