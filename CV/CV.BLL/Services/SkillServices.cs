19e75bd23134f6b9f8a1202e8334b5ec222a6fd9
max <maks.shamig.91@mail.ru>
2022-07-21 15:12:10 +0300

    added services for all skill models

---
+++ CV/CV.BLL/Services/SkillServices.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CV.BLL.Interfaces;
+using CV.BLL.Models;
+using CV.DAL.Entities;
+using CV.DAL.Interfaces;
+
+namespace CV.BLL.Services
+{
+    public class SkillServices : GenericServices<SkillModel, SkillEntity>, ISkillServices
+    {
+        public SkillServices(IGenericRepository<SkillEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
